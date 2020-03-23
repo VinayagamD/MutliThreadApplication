e19b49173e283b2e8f2202eb892e3c841f572107
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-03-23 22:05:20 +0530

    Added Multithread Initial Application Basics

---
+++ src/main/java/com/vinay/mutlithread/MainExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.vinay.mutlithread;
+
+public class MainExceptionHandler {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            // Code that will run in a new thread
+            throw new RuntimeException("Intentional Exception");
+        });
+        thread.setUncaughtExceptionHandler((t, e) -> {
+            System.out.println("A critical error happened in thread "+t.getName()
+                    + " the error is " + e.getMessage());
+        });
+
+        thread.start();
+    }
+}
