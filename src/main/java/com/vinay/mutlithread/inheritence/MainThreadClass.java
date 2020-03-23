c7352d5709c156d5e9048f4dcb7ff00c05602bd4
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-03-24 00:06:09 +0530

    Inheritinh Thread Class Example

---
+++ src/main/java/com/vinay/mutlithread/inheritence/MainThreadClass.java
@@ -0,0 +1,19 @@
+package com.vinay.mutlithread.inheritence;
+
+public class MainThreadClass {
+
+    public static void main(String[] args) {
+        Thread thread = new NewThread();
+
+        thread.start();
+    }
+
+    private static class NewThread extends Thread {
+
+        @Override
+        public void run() {
+            // Code that executes on the new thread
+            System.out.println("Hello from "+ this.getName());
+        }
+    }
+}
